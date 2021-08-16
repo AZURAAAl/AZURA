22d1a4740d7e56e7a8abeb2bdeea2dbe1c94d0d6
AZURAAAl <88915531+AZURAAAl@users.noreply.github.com>
2021-08-17 06:25:51 +0700

    Update upload-ovpn.sh

--- upload-ovpn.sh
+++ upload-ovpn.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo ""
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[0;32m-               MADE BY PHC_Jayvee               -\033[0m"
+echo -e "\033[0;32m-               AZURA                            -\033[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-echo "what file you want to move in web?(eg: test.ovpn): "
+echo "คุณต้องการย้ายไฟล์ใดในเว็บ (เช่น test.ovpn) "
 read file
 cp /root/$file /var/www/html
 
-echo "done"
+echo "เสร็จแล้ว"
